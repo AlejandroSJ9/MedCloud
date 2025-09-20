8aad5105f6a8f6ebcbf7fc65567fe23cf05fef41
AlejandroSJ9 <alejandro.salas090603@gmail.com>
2025-09-20 10:05:13 -0500

    first commit: Dependencies

---
+++ src/main/java/com/medcloud/app/MedCloudApplication.java
@@ -0,0 +1,13 @@
+package com.medcloud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medcloud/app/MedCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medcloud.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
